ad1cfa62012f0f9071d093914234e1690732f98b
AbdullahYounas <abdullahyounas24082005@gmail.com>
2025-02-13 12:32:52 +0500

    Security V1.0

--- sencdec/src/config/firebase.jsx
+++ sencdec/src/config/firebase.jsx
@@ -1,19 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaDKS31LvSqbBXvs-pICzH3C_i_IagkZQ",
-  authDomain: "sencdec.firebaseapp.com",
-  projectId: "sencdec",
-  storageBucket: "sencdec.appspot.com",
-  messagingSenderId: "776108973983",
-  appId: "1:776108973983:web:b3a7fd663f2db556cc67aa",
-  measurementId: "G-W03PS7T1XM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 export const db = getFirestore(app);
